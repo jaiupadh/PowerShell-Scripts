6935802f9fe498b5bb9b3c56d1d3da3d3895deb8
Jaidev Upadhya <97837505+jaiupadh@users.noreply.github.com>
2022-08-03 20:59:15 +0000

    Add files via upload

---
+++ MoveCursor.ps1
@@ -0,0 +1,15 @@
+##############################################################################
+# This is a PowerShell script to move cursor constantly		             #
+#                       Authored by JAIUPADH on 04/08/2022                   #
+############################################################################## 
+
+Add-Type -AssemblyName System.Windows.Forms
+
+while ($true)
+{
+  $Pos = [System.Windows.Forms.Cursor]::Position
+  $x = ($pos.X % 500) + 1
+  $y = ($pos.Y % 500) + 1
+  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+  Start-Sleep -Seconds 10
+}
